bc3f14290d8e7de997c9afab7149fc39afe0f576
David Luis Wiegandt <wigandtd@hu-berlin.de>
2023-04-12 15:38:35 +0200

    added more erros

--- collections/errors.go
+++ collections/errors.go
@@ -8,26 +8,46 @@ const (
 	// ErrEmptySliceParameter occurs when a function is called with an empty slice parameter where a non-empty slice is required.
 	ErrEmptySliceParameter
 
-	// ErrNotEnoughRangeParameters occurs when Range is called with no parameters.
-	ErrNotEnoughRangeParameters
+	// ErrNotEnoughParameters occurs when Range is called with no parameters.
+	ErrNotEnoughParameters
 
 	// ErrTooManyParameters occurs when Range is called with more than three parameters.
-	ErrTooManyRangeParameters
+	ErrTooManyParameters
 
 	// ErrRangeStepZero occurs when Range is called with a step size of zero.
 	ErrRangeStepIsZero
+
+	// ErrEmptyStream occurs when an empty stream is passed where a non-empty stream is required.
+	ErrEmptyStream
+
+	// ErrSliceRangeOutOfBounds occurs when either end of a sub-slice is outside the source slide's range.
+	ErrSliceRangeOutOfBounds
+
+	// ErrIndexOutOfBounds occurs when an accessed index refers to memory outside the structure's allocated memory.
+	ErrIndexOutOfBounds
+
+	// ErrNegativeCapacity occurs when a negative capacity is passed to a function that requires a non-negative capacity.
+	ErrNegativeCapacity
 )
 
 func (e CollectionError) Error() string {
 	switch e {
 	case ErrEmptySliceParameter:
 		return "empty slice parameter where a non-empty slice is required"
-	case ErrNotEnoughRangeParameters:
-		return "not enough parameters for Range"
-	case ErrTooManyRangeParameters:
-		return "too many parameters for Range"
+	case ErrNotEnoughParameters:
+		return "not enough parameters"
+	case ErrTooManyParameters:
+		return "too many parameters"
 	case ErrRangeStepIsZero:
 		return "step size of Range is zero"
+	case ErrEmptyStream:
+		return "empty stream where a non-empty stream is required"
+	case ErrSliceRangeOutOfBounds:
+		return "either end of a sub-slice is outside the source slide's range"
+	case ErrIndexOutOfBounds:
+		return "accessed index refers to memory outside the structure's allocated memory"
+	case ErrNegativeCapacity:
+		return "negative capacity where a non-negative capacity is required"
 	default:
 		return "unknown error"
 	}
